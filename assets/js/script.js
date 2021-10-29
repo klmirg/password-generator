2f3f31ba214c33deecdc5b7ba147d29b94e418b2
Ronya <klmirg6@gmail.com>
2021-10-29 02:00:43 -0500

    Getting the prompts to work properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,55 @@
 // Assignment code here
-var passwordLength = 8 > 128;
+var generatePassword = ["password length", "special characters", "numeric characters", "lower case", "upper case"];
+  
+var buttonEl = document.querySelector("#generate");
 
-var specialCharacters =  window.prompt("Do you want special characters?");
-var lowerCase = window.prompt("Do you want lower case letters?");
-var upperCase = window.prompt("Do you want upper case letters?");
+var passwordLength = ""
+var specialCharacters = ""
+var numericCharacters = ""
+var lowerCase = ""
+var upperCase = ""
 
+
+// var specialCharacters =  prompt("Do you want special characters?");
+// var lowerCase = prompt("Do you want any lower case letters?");
+// var upperCase = prompt("Do you want any upper case letters?"); 
+
+// Do you want special characters?
+// Do you want lower case or upper case?
+// How long do you want the password to be? between 8 and 128 characters
 
 var generatePassword = function() {
 
-  if (specialCharacters === true)
+  var passwordLength = prompt("How many characters would you like your password to contain?");
+    if ( passwordLength > 8 && passwordLength < 128) {
+      console.log(passwordLength);
+    } else  {
+      alert("You must choose between 8 and 128 characters.");
+      
+      while ( passwordLength < 8 || passwordLength > 128) {
+        passwordLength = prompt("How many characters would you like your password to contain>");
+     }
+    }
 
-}
+  var specialCharacters = window.confirm("Click OK to comfirm including special characters.");
+    if (specialCharacters);
+    console.log(specialCharacters);
+
+
+  var numericCharacters = window.confirm("Click OK to confirm including numeric characters.");
+    if (numericCharacters);
+    console.log(numericCharacters);
+
+  var lowerCase = window.confirm("Click OK to confirm including lower case letters.");
+    if (lowerCase);
+    console.log(lowerCase);
+
+  var upperCase = window.confirm("Click OK to confirm including upper case letters.");
+    if (upperCase);
+    console.log(upperCase);
+
+  };
+
 
 
 // Get references to the #generate element
